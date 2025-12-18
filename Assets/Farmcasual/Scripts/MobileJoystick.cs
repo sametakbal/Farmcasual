d25c48bbe69aae37355b44033385a14a6faec16b
Samet Akbal <39596965+sametakbal@users.noreply.github.com>
2025-12-18 23:28:24 +0300

    Add initial project settings and configuration files
    - Created SceneTemplateSettings.json to define template pin states and dependency type information.
    - Added ShaderGraphSettings.asset for shader graph configurations.
    - Introduced TagManager.asset to manage tags and layers in the project.
    - Established TimeManager.asset for managing time settings in the project.
    - Implemented URPProjectSettings.asset for Universal Render Pipeline settings.
    - Configured UnityConnectSettings.asset for Unity services and analytics.
    - Added VFXManager.asset for managing visual effects settings.
    - Created VersionControlSettings.asset to manage version control settings.
    - Introduced XRSettings.asset for XR device settings.
    - Added ignore.conf to specify files and directories to be ignored by version control.

---
+++ Assets/Farmcasual/Scripts/MobileJoystick.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MobileJoystick : MonoBehaviour
+{
+
+    [Header(" Elements ")]
+    [SerializeField] private RectTransform joystickOutline;
+    [SerializeField] private RectTransform joystickKnob;
+
+    [Header(" Settings ")]
+    [SerializeField] private bool canControl;
+    [SerializeField] private float moveFactor = 50f;
+
+    private Vector3 clickedPosition;
+
+
+    void Start()
+    {
+        HideJoystick();
+    }
+
+    void Update()
+    {
+        if (canControl)
+        {
+            ControlJoystick();
+        }
+    }
+
+    public void ClickedOnJoystickZoneCallback()
+    {
+        clickedPosition = Pointer.current.position.ReadValue();
+        joystickOutline.position = clickedPosition;
+        ShowJoystick();
+    }
+
+    private void ShowJoystick()
+    {
+        joystickOutline.gameObject.SetActive(true);
+        canControl = true;
+    }
+
+    private void HideJoystick()
+    {
+        joystickOutline.gameObject.SetActive(false);
+        canControl = false;
+    }
+
+    private void ControlJoystick()
+    {
+        Vector3 currentPosition = Pointer.current.position.ReadValue();
+        Vector3 direction = currentPosition - clickedPosition;
+
+        float moveMagnitude = direction.magnitude * moveFactor / Screen.width;
+
+        moveMagnitude = Mathf.Min(moveMagnitude, joystickOutline.rect.width / 2);
+
+        Vector3 move = direction.normalized * moveMagnitude;
+        Vector3 targetPosition = clickedPosition + move;
+        joystickKnob.position = targetPosition;
+
+        if (!Pointer.current.press.isPressed)
+        {
+            HideJoystick();
+        }
+
+
+
+    }
+}
